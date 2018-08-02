bd6e24c1ba8ebc533410cedd32aad76b0e77494c
sp00kygh0st <abc@def.xyz>
2018-08-02 08:41:04 -0400

    dummy webapp package

--- pkg/xyzzy/thing.go
+++ pkg/xyzzy/thing.go
@@ -1,4 +1,23 @@
 package xyzzy
 
-func DoSomething() {
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Xyzzy struct {
+	engine *gin.Engine
+}
+
+func (x *Xyzzy) Index(c *gin.Context) {
+	c.String(200, "OK")
+}
+
+func (x *Xyzzy) Run(addr string) {
+	x.engine.Run(addr)
+}
+
+func NewXyzzy() *Xyzzy {
+	x := new(Xyzzy)
+	x.engine = gin.Default()
+	return x
 }
