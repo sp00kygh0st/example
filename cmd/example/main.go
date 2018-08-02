c512d2abaa4fc761b2dbc1ecfc083462b55b1f69
sp00kygh0st <abc@def.xyz>
2018-08-02 08:41:32 -0400

    consume new dummy package

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/sp00kygh0st/example/pkg/foobar"
+	"github.com/sp00kygh0st/example/pkg/xyzzy"
 )
 
 func main() {
 	foobar.Foobar()
+
+	x := xyzzy.NewXyzzy()
+	x.Run()
 }
